0d2f247293e56f796fb81ed12d7cee41c560c33d
Gosha <guerosf@yandex.ru>
2017-04-01 19:27:34 +0300

    переделано под структуру запроса

--- first_page.html.php
+++ first_page.html.php
@@ -9,21 +9,22 @@
 <body>
 <header><span>Учёт</span></header>
 <content>
+<?php for($i=0;$i<4;$i++): ?>
 	<div class="month">
-		<div class="month_name"><span><?php echo $month2Last['name'];?></span></div>
+		<div class="month_name"><span><?php echo $monthDisplay[$i]['name'];?></span></div>
 		<div class="salary">
 			<table>
 				<tr>
 					<td colspan="2">офицально</td>
 				</tr>
 				<tr>
-					<td>15000</td><td>из 15000</td>
+					<td><?php echo $moneyCat[1][$i];?></td><td>из 15000</td>
 				</tr>
 				<tr>
 					<td colspan="2">не офицально</td>
 				</tr>
 				<tr>
-					<td>100000</td><td>из 100000</td>
+					<td><?php echo $moneyCat[2][$i];?></td><td>из 100000</td>
 				</tr>
 			</table>
 		</div>
@@ -36,135 +37,19 @@
 					<td>Оплачено</td>
 				</tr>
 				<tr>
-					
+					<td><?php echo $moneyCat[3][$i];?></td>
 				</tr>
 			</table>
 		</div>
 		<div class="view_detail">
 			<form>
-				<input type="hidden" name="month" value="<?php echo $month2Last['num'];?>">
+				<input type="hidden" name="month" value="<?php echo $monthDisplay[$i]['num'];?>">
 				<input type="hidden" name="swallow" value="edit">
 				<input type="submit" value="Просмотр">
 			</form>
 		</div>
 	</div>
-		<div class="month">
-		<div class="month_name"><span><?php echo $month1Last['name'];?></span></div>
-		<div class="salary">
-			<table>
-				<tr>
-					<td colspan="2">офицально</td>
-				</tr>
-				<tr>
-					<td>15000</td><td>из 15000</td>
-				</tr>
-				<tr>
-					<td colspan="2">не офицально</td>
-				</tr>
-				<tr>
-					<td>100000</td><td>из 100000</td>
-				</tr>
-			</table>
-		</div>
-		<div class="swallow">
-			<table>
-				<tr>
-					<th>Ласточка</th>
-				</tr>
-				<tr>
-					<td>Оплачено</td>
-				</tr>
-				<tr>
-					
-				</tr>
-			</table>
-		</div>
-		<div class="view_detail">
-			<form>
-				<input type="hidden" name="month" value="<?php echo $month1Last['num'];?>">
-				<input type="hidden" name="swallow" value="edit">
-				<input type="submit" value="Просмотр">
-			</form>
-		</div>
-	</div>
-		<div class="month">
-		<div class="month_name"><span><?php echo $monthToday['name'];?></span></div>
-		<div class="salary">
-			<table>
-				<tr>
-					<td colspan="2">офицально</td>
-				</tr>
-				<tr>
-					<td>15000</td><td>из 15000</td>
-				</tr>
-				<tr>
-					<td colspan="2">не офицально</td>
-				</tr>
-				<tr>
-					<td>100000</td><td>из 100000</td>
-				</tr>
-			</table>
-		</div>
-		<div class="swallow">
-			<table>
-				<tr>
-					<th>Ласточка</th>
-				</tr>
-				<tr>
-					<td>Оплачено</td>
-				</tr>
-				<tr>
-					
-				</tr>
-			</table>
-		</div>
-		<div class="view_detail">
-			<form>
-				<input type="hidden" name="month" value="<?php echo $monthToday['num'];?>">
-				<input type="hidden" name="swallow" value="edit">
-				<input type="submit" value="Просмотр">
-			</form>
-		</div>
-	</div>
-		<div class="month">
-		<div class="month_name"><span><?php echo $monthNext['name'];?></span></div>
-		<div class="salary">
-			<table>
-				<tr>
-					<td colspan="2">офицально</td>
-				</tr>
-				<tr>
-					<td>15000</td><td>из 15000</td>
-				</tr>
-				<tr>
-					<td colspan="2">не офицально</td>
-				</tr>
-				<tr>
-					<td>100000</td><td>из 100000</td>
-				</tr>
-			</table>
-		</div>
-		<div class="swallow">
-			<table>
-				<tr>
-					<th>Ласточка</th>
-				</tr>
-				<tr>
-					<td>Оплачено</td>
-				</tr>
-				<tr>
-					
-				</tr>
-			</table>
-		</div>
-		<div class="view_detail">
-			<form>
-				<input type="hidden" name="month" value="<?php echo $monthNext['num'];?>">
-				<input type="hidden" name="swallow" value="edit">
-				<input type="submit" value="Просмотр">
-			</form>
-		</div>
-	</div>
+<?php endfor; ?>
 </content>
 </body>
 </html>
