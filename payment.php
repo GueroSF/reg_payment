94e2c7abbf1723b200eed16f2999043b0e302203
Gosha <guerosf@yandex.ru>
2017-04-01 20:25:02 +0300

    Запрос о платежах за месяц

--- payment.php
+++ payment.php
@@ -3,5 +3,17 @@ include $_SERVER['DOCUMENT_ROOT'].'reg/includes/path.inc.php';
 include $pathFileInc.'db_connect.inc.php';
 include $pathFileInc.'error.inc.php';
 if (isset($_POST['month_payment'])&&$_POST['month_payment']=='edit'){
-	
+	$month = $_POST['month'];
+	try {
+		$sql = 'SELECT p.money, c.name, p.payment_for_month pm, p.payment_date pd FROM payment p
+				INNER JOIN payment_category c ON c.id = p.payment_category
+				WHERE p.payment_for_month = :month';
+		$r = $pdo -> prepare($sql);
+		$r -> bindValue(':month', $month);
+		$r -> execute();
+	} catch (PDOException $e) {
+		errorMessage('Ошибка при извлечении платежей за месяц');
+	}
+	$paymentMonth = $r->fetchALL();
+	var_dump($paymentMonth);
 }
