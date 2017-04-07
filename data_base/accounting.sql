f6f08749d9a1eb6c764bf048ea3e8e0fe28c417b
Gosha <guerosf@yandex.ru>
2017-04-07 12:53:51 +0300

    Выбраны типы TINYINT для некоторых столбцов id

--- data_base/accounting.sql
+++ data_base/accounting.sql
@@ -1,37 +1,62 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
---
--- Хост: localhost
--- Время создания: Апр 03 2017 г., 10:49
--- Версия сервера: 5.6.12-log
--- Версия PHP: 5.4.16
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- База данных: `accounting`
---
 CREATE DATABASE IF NOT EXISTS `accounting` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `accounting`;
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `buh_account` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Структура таблицы `payment`
---
+
+CREATE TABLE IF NOT EXISTS `buh_category` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `buh_comment_payment` (
+  `transaction_id` int(11) NOT NULL,
+  `comment` varchar(100) NOT NULL,
+  KEY `transaction_id` (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `buh_operation` (
+  `id` tinyint(1) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+INSERT INTO `buh_operation` (`id`, `name`) VALUES
+(1, 'Приход'),
+(2, 'Расход');
+
+CREATE TABLE IF NOT EXISTS `buh_transaction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` tinyint(3) unsigned NOT NULL,
+  `operations` tinyint(2) unsigned NOT NULL,
+  `category` tinyint(3) unsigned NOT NULL,
+  `money` decimal(10,2) NOT NULL,
+  `date_operations` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account` (`account`),
+  KEY `operations` (`operations`),
+  KEY `category` (`category`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
 
 CREATE TABLE IF NOT EXISTS `payment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `money` decimal(7,2) NOT NULL,
-  `payment_category` int(1) NOT NULL,
+  `money` decimal(10,2) NOT NULL,
+  `payment_category` tinyint(3) unsigned NOT NULL,
   `Payment_for_month` varchar(7) NOT NULL,
   `Payment_date` date NOT NULL,
   PRIMARY KEY (`id`),
@@ -39,55 +64,42 @@ CREATE TABLE IF NOT EXISTS `payment` (
   KEY `payment_category` (`payment_category`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Дамп данных таблицы `payment`
---
-
-INSERT INTO `payment` (`id`, `money`, `payment_category`, `Payment_for_month`, `Payment_date`) VALUES
-(1, 100, 1, '2_2017', '2017-04-03'),
-(2, 3000, 2, '2_2017', '2017-04-03'),
-(3, 350, 3, '2_2017', '2017-04-03'),
-(4, 2000, 2, '3_2017', '2017-04-03'),
-(5, 320, 3, '3_2017', '2017-04-03');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `payment_category`
---
 
 CREATE TABLE IF NOT EXISTS `payment_category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
-
---
--- Дамп данных таблицы `payment_category`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 INSERT INTO `payment_category` (`id`, `name`) VALUES
 (1, 'Офицальная'),
 (2, 'Неофицальная'),
 (3, 'Ласточка');
 
---
--- Ограничения внешнего ключа сохраненных таблиц
---
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `pass` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
---
--- Ограничения внешнего ключа таблицы `payment`
---
+INSERT INTO `user` (`id`, `name`, `pass`) VALUES
+(1, '1', 'c4ca4238a0b923820dcc509a6f75849b'),
+(2, '2', 'c4ca4238a0b923820dcc509a6f75849b');
+
+
+ALTER TABLE `buh_comment_payment`
+  ADD CONSTRAINT `buh_comment_payment_ibfk_1` FOREIGN KEY (`transaction_id`) REFERENCES `buh_transaction` (`id`);
+
+ALTER TABLE `buh_transaction`
+  ADD CONSTRAINT `buh_transaction_ibfk_3` FOREIGN KEY (`category`) REFERENCES `buh_category` (`id`),
+  ADD CONSTRAINT `buh_transaction_ibfk_1` FOREIGN KEY (`account`) REFERENCES `buh_account` (`id`),
+  ADD CONSTRAINT `buh_transaction_ibfk_2` FOREIGN KEY (`operations`) REFERENCES `buh_operation` (`id`);
+
 ALTER TABLE `payment`
   ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`payment_category`) REFERENCES `payment_category` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `pass` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
