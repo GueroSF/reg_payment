325d228c941771d97a2d23650cfd37f24c884a1f
Gosha <guerosf@yandex.ru>
2017-04-13 09:16:58 +0300

    замена <?php echo на <?=

--- buh/account_page.html.php
+++ buh/account_page.html.php
@@ -1,18 +1,18 @@
 <?php foreach ($accounts as $acc): ?>
 	<div class="month">
-		<div class="month_name"><span><?php echo $acc['name'];?></span></div>
+		<div class="month_name"><span><?= $acc['name'];?></span></div>
 		<div class="salary">
 			<table>
 				<tr>
 					<td>Всего</td>
 				</tr>
 				<tr>
-					<td><?php echo $acc['sum'];?></td>
+					<td><?= $acc['sum'];?></td>
 				</tr>
 			</table>
 		</div>
 		<div class="view_detail">
-			<a href="?account=<?php echo $acc['id'];?>">Просмотр</a>	
+			<a href="?account=<?= $acc['id'];?>">Просмотр</a>	
 		</div>
 	</div>
 <?php endforeach; ?>
