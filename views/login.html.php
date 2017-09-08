b6071cd58da55b35e7ca5e7d214013d742d02707
Gosha <guerosf@yandex.ru>
2017-09-08 13:45:39 +0300

    add manifest.json for android

--- views/login.html.php
+++ views/login.html.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="apple-touch-icon" sizes="180x180" href="http://<?= $_SERVER['HTTP_HOST'] ?>/icon/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="http://<?= $_SERVER['HTTP_HOST'] ?>/icon/favicon-32x32.png" sizes="32x32">
+    <link rel="manifest" href="http://<?= $_SERVER['HTTP_HOST'] ?>/manifest.json">
     <title><?= $this->title ?></title>
     <link rel="stylesheet" type="text/css" href="http://<?= $_SERVER['HTTP_HOST'] ?>/style/login.css">
 </head>
