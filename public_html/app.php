b2d005e0a58b3ddcd4cd180aba815de61f34cab0
Gosha <guerosf@yandex.ru>
2018-04-06 17:32:20 +0300

    added docker

---
+++ public_html/app.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date: 06.04.18
+ * Time: 17:25
+ */
+
+mb_internal_encoding('utf-8');
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once 'index.php';
