8b6d0d3f5dd73f49dd9798a8c8a6d1fea1f055d3
Gosha <guerosf@yandex.ru>
2018-08-06 18:17:09 +0300

    little changes

--- public_html/js/category.ts
+++ public_html/js/category.ts
@@ -1,44 +1,56 @@
 'use strict';
-var Button = /** @class */ (function () {
-    function Button(name) {
+
+class Button {
+    name: string;
+    action: boolean;
+    divButton: HTMLDivElement;
+
+    constructor(name: string) {
         this.name = name;
         this.init();
         this.addEvent();
     }
-    Button.prototype.init = function () {
-        this.divButton = document.querySelector('.' + this.name);
+
+    private init() {
+        this.divButton = document.querySelector('.'+ this.name);
         this.action = this.divButton !== null;
-    };
-    Button.prototype.addEvent = function () {
+    }
+
+    private addEvent() {
         if (!this.action) {
             return;
         }
-        var self = this;
+        let self = this;
         this.divButton.querySelector('button').addEventListener('click', function (e) {
             document.querySelectorAll('.month').forEach(function (elem) {
                 elem.classList.remove('hide');
                 self.hide();
-            });
-        });
-    };
-    Button.prototype.hide = function () {
+            })
+        })
+    }
+
+    private hide() {
         if (!this.action) {
             return;
         }
         this.divButton.classList.add('hide');
-    };
-    Button.prototype.hideIfNull = function () {
+    }
+
+    hideIfNull() {
         if (!this.action) {
             return;
         }
-        var hideMonth = document.querySelectorAll('.hide');
+
+        let hideMonth = document.querySelectorAll('.hide');
+
         if (hideMonth.length === 0) {
             this.hide();
         }
-    };
-    return Button;
-}());
+
+    }
+}
+
 window.addEventListener('load', function () {
-    var b = new Button('button_for_view_if_zero');
+    let b = new Button('button_for_view_if_zero');
     b.hideIfNull();
 });
