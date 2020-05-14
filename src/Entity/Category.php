a5511ca14cfc55d924599db9d5459204ad2827cb
gosha <guerosf@yandex.ru>
2020-05-14 10:57:33 +0300

    #command use interface for dictionary

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Lib\Interfaces\DictionaryInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -11,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="category")
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
  */
-class Category extends AbstractBaseEntity
+class Category extends AbstractBaseEntity implements DictionaryInterface
 {
     /**
      * @ORM\Column(name="name", type="string", length=25, nullable=false)
